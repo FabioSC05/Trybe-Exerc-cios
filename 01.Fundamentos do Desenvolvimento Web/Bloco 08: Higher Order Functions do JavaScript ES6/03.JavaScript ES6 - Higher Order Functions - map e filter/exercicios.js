688019afa17d7fc0307a81f9a5b210ede81272e5
FabioSC <fabioscarvalh@yahoo.com.br>
2022-05-17 21:00:36 -0300

    Atualização 17/05/2022 21:00

---
+++ 01.Fundamentos do Desenvolvimento Web/Bloco 08: Higher Order Functions do JavaScript ES6/03.JavaScript ES6 - Higher Order Functions - map e filter/exercicios.js
@@ -0,0 +1,94 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+function formatedBookNames(array) {
+  return array.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+}
+
+function nameAndAge(array) {
+  return array.map((element) => {
+    return { age: element.releaseYear - element.author.birthYear, author: element.author.name }
+  }).sort((a, b) => a.age - b.age);
+}
+
+function fantasyOrScienceFiction(array) {
+  return array.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
+}
+
+function oldBooksOrdered(array) {
+  return array.filter((book) => 2022 - book.releaseYear > 60).sort((a, b) => a.releaseYear - b.releaseYear);
+}
+
+function fantasyOrScienceFictionAuthors(array) {
+  return fantasyOrScienceFiction(array).map((element) => `${element.author.name}`).sort();
+}
+
+function oldBooks(array) {
+  return array.filter((book) => 2022 - book.releaseYear > 60).map((nome) => `${nome.name}`);
+}
+
+function authorWith3DotsOnName(array) {
+  let livro;
+  array.filter((nome) => nome.author.name[1] === '.' && nome.author.name[4] === '.' && nome.author.name[7] === '.').forEach((element) => livro = element.name);
+  return livro;
+}
